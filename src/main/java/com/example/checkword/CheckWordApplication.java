eed7c9242289a22ef4ef08525064c2c2c2527796
studioym-bb <studioym.bb@gmail.com>
2026-06-25 10:21:14 +0900

    checkWord first setting

---
+++ src/main/java/com/example/checkword/CheckWordApplication.java
@@ -0,0 +1,13 @@
+package com.example.checkword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckWordApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CheckWordApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/checkword/CheckWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckWordApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
